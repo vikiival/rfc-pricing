a503781fa73e3f6e17b2e2375d0ebcccd505b596
Aurora Poppyseed <aurora.makovac@gmail.com>
2023-12-12 12:44:48 +0100

    adding titles

--- tests/deposit.test.js
+++ tests/deposit.test.js
@@ -4,14 +4,17 @@ import * as x from '../index'
 describe('deposit', () => {
   it('should calculate collection deposit', async () => {
     const result = await x.calculateCollectionDeposit()
+    console.log("Collection deposit: ")
     console.log(result)
   })
   it('should calculate token deposit', async () => {
     const result = await x.calculateTokenDeposit()
+    console.log("Token deposit: ")
     console.log(result)
   })
   it('should calculate collection metadata deposit', async () => {
     const result = await x.calculateCollectionMetadataDeposit()
+    console.log("Metadata deposit: ")
     console.log(result)
   })
 })
