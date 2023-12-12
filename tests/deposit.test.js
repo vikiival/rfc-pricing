a1b34ca8b1b212514c4adede66893685938c0bae
Viki Val <viktorko99@gmail.com>
2023-12-12 11:33:09 +0100

    :test_tube: Values are compared to on-chain variables

--- tests/deposit.test.js
+++ tests/deposit.test.js
@@ -1,17 +1,20 @@
-import { describe, it } from 'vitest'
+import { describe, test as it, expect } from 'vitest'
 import * as x from '../index'
 
 describe('deposit', () => {
   it('should calculate collection deposit', async () => {
     const result = await x.calculateCollectionDeposit()
     console.log(result)
+    expect.soft(result.human).toBeLessThan(10)
   })
   it('should calculate token deposit', async () => {
     const result = await x.calculateTokenDeposit()
     console.log(result)
+    expect.soft(result.human).toBeLessThan(0.01)
   })
   it('should calculate collection metadata deposit', async () => {
     const result = await x.calculateCollectionMetadataDeposit()
     console.log(result)
+    expect.soft(result.human).toBeLessThan(0.20129)
   })
 })
